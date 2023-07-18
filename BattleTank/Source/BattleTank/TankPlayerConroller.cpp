0044437b2ae0b0c8cf87b8336dbf1f21b0d5dabc
aktorionoff@gmail.com <aktorionoff@gmail.com>
2023-07-18 20:58:26 +0300

    bt122

--- BattleTank/Source/BattleTank/TankPlayerConroller.cpp
+++ BattleTank/Source/BattleTank/TankPlayerConroller.cpp
@@ -3,6 +3,20 @@
 #include "BattleTank.h"
 #include "TankPlayerConroller.h"
 
+void ATankPlayerConroller::BeginPlay()
+{
+	Super:: BeginPlay();
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()));
+
+	}
+
+}
 ATank* ATankPlayerConroller::GetControlledTank() const 
 {
 	return Cast<ATank>(GetPawn());
--- BattleTank/Source/BattleTank/TankPlayerConroller.h
+++ BattleTank/Source/BattleTank/TankPlayerConroller.h
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-#include "Tank.h"
+#include "Public/Tank.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerConroller.generated.h" // Must be the last include
 
@@ -16,6 +16,6 @@ class BATTLETANK_API ATankPlayerConroller : public APlayerController
 public:
 	ATank* GetControlledTank() const;
 
-	
+	virtual void BeginPlay() override;
 	
 };
