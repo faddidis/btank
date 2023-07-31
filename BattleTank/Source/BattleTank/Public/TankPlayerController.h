d1462d09f50a18ecdb0d9f366d452441cda45deb
aktorionoff@gmail.com <aktorionoff@gmail.com>
2023-07-31 22:11:45 +0300

    bt131

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // Must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+private:
+	ATank* GetControlledTank() const;
+
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+	
+	// Start the tank moving the barrel so that a shot would it where the crosshair intersects the world;
+	void AimTowardsCrosshair();
+
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayLocation(FVector& HitLocation) const;
+
+	
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+
+};
